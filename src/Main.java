379ba4a256a79a65e9229053d980b182b31f79ae
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-05-17 00:38:32 +0530

    first commit

---
+++ src/Main.java
@@ -0,0 +1,19 @@
+public class Main {
+    public static void main(String[] args) {
+
+        // store 5 numbers
+        int[] number = new int[5];
+        number[1] = 12;
+
+        // store 5 names
+        String[] names = new String[5];
+
+        // Classes are similar to above type of data types
+        // So Classes are same to data types
+        // Class is a data type with different properties and methods
+
+
+        System.out.println(number[1]);
+    }
+
+}
