9964a2ea294efff7cff67c669e2e557d334d8a3c
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-05-17 23:28:17 +0530

    4th test

--- src/Main.java
+++ src/Main.java
@@ -14,19 +14,26 @@ public class Main {
         // So Classes are same to data types
         // Class is a data type with different properties and methods
 
-
 //        System.out.println(number[1]);
 
         // declare
         Student lahiru = new Student();
+//        lahiru.greeting();
+
+        Student kasun = new Student(12,"kasun padmilar", 46.5f);
+
+        System.out.println(kasun.sno);
+        System.out.println(kasun.name);
+        System.out.println(kasun.marks);
+        kasun.greeting();
 
 //        lahiru.sno = 123;
 //        lahiru.name= "Lahiru C.";
 //        lahiru.marks = 56.5F;
 
-        System.out.println(lahiru.name);
-        System.out.println(lahiru.marks);
-        System.out.println(lahiru.sno);
+//        System.out.println(lahiru.name);
+//        System.out.println(lahiru.marks);
+//        System.out.println(lahiru.sno);
     }
 
 }
@@ -37,10 +44,20 @@ class Student {
     String name;
     float marks;
 
+    void greeting() {
+        System.out.println("Welcome " + this.name);
+    }
+
     Student () {
         this.sno = 13;
         this.name = "LC";
         this.marks = 34.5f;
+    }
+
+    Student (int sno, String name, float marks) {
+        this.sno = sno;
+        this.name = name;
+        this.marks = marks;
     }
 
 }
