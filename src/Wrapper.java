61909a4c38f117f7b8846da3e0e0aa874df3a524
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-05-18 15:25:18 +0530

    'final' in primitive data type and class..

--- src/Wrapper.java
+++ src/Wrapper.java
@@ -5,6 +5,27 @@ public class Wrapper {
 
         Integer num = 5;
 
+        final Child vish = new Child("vish");
+        // in this case we can modify "vish" object because
+        // this final not used in the primitive data type
+        vish.name = "vishwajith";
+
+        System.out.println(vish.name);
+
+        // but can not reassign that object
+//        vish = new Child("dauifaf"); // error occur
 
     }
+}
+
+class Child {
+    // when final using primitive data type, it is not permitting to change that value of
+    // final variable and, it should be initialized at first stage
+    final int age = 5;
+    String name;
+
+    Child(String name) {
+        this.name = name;
+    }
+
 }
