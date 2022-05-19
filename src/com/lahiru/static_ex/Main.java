a7c9b3772d46188bc6491485db77485693fd1ceb
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-05-19 19:47:17 +0530

    static(not related to an object, non-static(related to an object)

--- src/com/lahiru/static_ex/Main.java
+++ src/com/lahiru/static_ex/Main.java
@@ -6,8 +6,43 @@ public class Main {
         Human kasun = new Human("Kasun", 22, 25500, 25867);
         Human isuru = new Human("Isuru", 22, 500000, 23456);
 
-        System.out.println(lahiru.population);
-        System.out.println(kasun.population);
+        System.out.println(Human.population);
+        System.out.println(Human.population);
+
+        Main something = new Main();
+        something.greeting();
+        something.nonStaticFunction();
+
+        staticFunction();
     }
 
+    // static function is not relating to an object/ instance
+    static void staticFunction() {
+        System.out.println("This is an static function");
+
+//        nonStaticFunction(); // inside a static function you can not run a non-static function
+        // because non-static function is relating to an object
+        // so, it should have an object to run byu static function do not n eed an object
+        // so non-static function can not run inside a static function
+
+        // but you can access to a non-static stuff (function or a variable) using an object
+        Main some = new Main();
+        some.nonStaticFunction();
+
+        Main some2 = new Main();
+        some2.greeting();
+
+    }
+
+    // non-static function belongs or relate to an object or an instance // (instance of a class and object are same)
+    void nonStaticFunction() {
+        System.out.println("This is a non-static function");
+        greeting(); // can run a non-static function inside a static function
+        // because the nonStaticFunction() will create an object(because non-static function belong to an object) and
+        // that object will refer to the greeting() function, so it also belongs to a object
+    }
+
+    void greeting() {
+        System.out.println("Welcome");
+    }
 }
