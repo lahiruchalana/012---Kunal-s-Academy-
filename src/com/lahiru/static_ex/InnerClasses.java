70d77f9bb7a694ee661271f0a6fb7a55471369f3
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-05-20 10:16:29 +0530

    change object's default functions

--- src/com/lahiru/static_ex/InnerClasses.java
+++ src/com/lahiru/static_ex/InnerClasses.java
@@ -8,14 +8,22 @@ public class InnerClasses {
         public Test(String name) {
             this.name = name;
         }
+
+        @Override
+        public String toString() {
+            return name;
+        }
+
     }
 
     public static void main(String[] args) {
         Test a = new Test("lahiru");
         Test b = new Test("kasun");
 
-        System.out.println(a.name);
-        System.out.println(b.name);
+//        System.out.println(a.name);
+//        System.out.println(b.name);
+
+        System.out.println(a);
 
 
     }
