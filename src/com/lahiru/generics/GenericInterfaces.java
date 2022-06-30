b5a0a0ece254e0bc888c6e4e7c35f3348e12492b
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-07-01 01:44:57 +0530

    generic interface

---
+++ src/com/lahiru/generics/GenericInterfaces.java
@@ -0,0 +1,5 @@
+package com.lahiru.generics;
+
+public interface GenericInterfaces<T> {
+    void sendData(T value);
+}
---
+++ src/com/lahiru/generics/Main.java
@@ -0,0 +1,10 @@
+package com.lahiru.generics;
+
+public class Main implements GenericInterfaces<Integer> {
+
+    @Override
+    public void sendData(Integer value) {
+        // some code
+    }
+
+}
