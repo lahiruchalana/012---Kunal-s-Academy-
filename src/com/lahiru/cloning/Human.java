0367ec06a78ca84bfc59257381e0ecbacff560fa
lahiruchalana <lahiruchalanairesh@gmail.com>
2022-07-12 19:06:04 +0530

    shallow copy

--- src/com/lahiru/cloning/Human.java
+++ src/com/lahiru/cloning/Human.java
@@ -3,10 +3,12 @@ package com.lahiru.cloning;
 public class Human implements Cloneable {
     int age;
     String name;
+    int[]  arr;
 
     public Human(int age, String name) {
         this.age = age;
         this.name = name;
+        this.arr = new int[]{1, 2, 4, 67, 23};
     }
 
     @Override
--- src/com/lahiru/cloning/Main.java
+++ src/com/lahiru/cloning/Main.java
@@ -1,11 +1,18 @@
 package com.lahiru.cloning;
 
+import java.util.Arrays;
+
 public class Main {
     public static void main(String[] args) throws CloneNotSupportedException {
         Human lahiru = new Human(25, "Lahiru");
 
         Human twin = (Human) lahiru.clone();
 
+        System.out.println(Arrays.toString(lahiru.arr));
+
+        twin.arr[0] = 100;
+
         System.out.println(twin.age + ", " + twin.name);
+        System.out.println(Arrays.toString(lahiru.arr));
     }
 }
